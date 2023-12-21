211f8e1714512ca5b22010911ecf764cfc61b754
Pratik Shastrakar <pratikshastrakar99@gmail.com>
2023-12-21 15:38:34 +0530

    Experience and Education UI changed

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".nav-menu");
-
-function toggleMenu() {
-	menu.classList.toggle("active");
-
-	if (menu.classList.contains("active")) {
-		// add x (close menu) icon
-		toggle.innerHTML = `<i class="fas fa-times"></i>`;
-	} else {
-		// add menu icon
-		toggle.innerHTML = `<i class="fas fa-bars"></i>`;
-	}
-}
-
-toggle.addEventListener("click", toggleMenu, false);
-
-// Add code to dynamically update the copyright year
 document.addEventListener("DOMContentLoaded", function () {
 	const currentYear = new Date().getFullYear();
 	const yearElement = document.getElementById("current-year");
@@ -23,4 +5,40 @@ document.addEventListener("DOMContentLoaded", function () {
 	if (yearElement) {
 		yearElement.innerText = currentYear;
 	}
+
+	// Toggle functionality for menu
+	const toggle = document.querySelector(".toggle");
+	const menu = document.querySelector(".nav-menu");
+
+	function toggleMenu() {
+		menu.classList.toggle("active");
+
+		if (menu.classList.contains("active")) {
+			// add x (close menu) icon
+			toggle.innerHTML = `<i class="fas fa-times"></i>`;
+		} else {
+			// add menu icon
+			toggle.innerHTML = `<i class="fas fa-bars"></i>`;
+		}
+	}
+
+	toggle.addEventListener("click", toggleMenu, false);
+
+	// Toggle functionality for details
+	const expWindows = document.querySelectorAll(".exp-window");
+
+	expWindows.forEach((expWindow) => {
+		const companyTile = expWindow.querySelector(".company-tile");
+		const targetId = companyTile.getAttribute("data-target");
+
+		if (companyTile && targetId) {
+			companyTile.addEventListener("click", function () {
+				const details = expWindow.querySelector(`#${targetId}-details`);
+
+				if (details) {
+					details.classList.toggle("open");
+				}
+			});
+		}
+	});
 });
